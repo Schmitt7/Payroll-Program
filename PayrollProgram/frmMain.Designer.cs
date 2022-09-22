af076e24fd053788b488ff6466859b2a27fc2094
schmitt7 <spencer.schmitt@live.com>
2022-09-22 17:50:07 -0400

    Add project files.

---
+++ PayrollProgram/frmMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace PayrollProgram
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txtHourlyRate = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.btnCalculatePay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(103, 56);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(182, 25);
+            this.lblHourlyRate.TabIndex = 0;
+            this.lblHourlyRate.Text = "Enter Hourly Rate";
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(339, 56);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(206, 25);
+            this.lblHoursWorked.TabIndex = 1;
+            this.lblHoursWorked.Text = "Enter Hours Worked";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(103, 287);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(70, 25);
+            this.lblDisplay.TabIndex = 2;
+            this.lblDisplay.Text = "label1";
+            // 
+            // txtHourlyRate
+            // 
+            this.txtHourlyRate.Location = new System.Drawing.Point(108, 113);
+            this.txtHourlyRate.Name = "txtHourlyRate";
+            this.txtHourlyRate.Size = new System.Drawing.Size(177, 31);
+            this.txtHourlyRate.TabIndex = 3;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(344, 113);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(201, 31);
+            this.txtHoursWorked.TabIndex = 4;
+            // 
+            // btnCalculatePay
+            // 
+            this.btnCalculatePay.Location = new System.Drawing.Point(108, 194);
+            this.btnCalculatePay.Name = "btnCalculatePay";
+            this.btnCalculatePay.Size = new System.Drawing.Size(177, 37);
+            this.btnCalculatePay.TabIndex = 5;
+            this.btnCalculatePay.Text = "Calculate Pay";
+            this.btnCalculatePay.UseVisualStyleBackColor = true;
+            this.btnCalculatePay.Click += new System.EventHandler(this.btnCalculatePay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(344, 194);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(177, 37);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(608, 194);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(177, 37);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 508);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculatePay);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtHourlyRate);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Name = "form1";
+            this.Text = "Spencer Schmitt - Employee Pay Calculator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txtHourlyRate;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.Button btnCalculatePay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PayrollProgram/frmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayrollProgram
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHourlyRate.Text = String.Empty;
+            txtHoursWorked.Text = String.Empty;
+        }
+
+        private void btnCalculatePay_Click(object sender, EventArgs e)
+        {
+
+            decimal WorkedHours = Convert.ToDecimal(txtHoursWorked.Text);
+            decimal HrRate = Convert.ToDecimal(txtHourlyRate.Text);
+            decimal gross = WorkedHours * HrRate;
+
+            decimal State = gross * .035m;
+            decimal Federal = gross * .15m;
+            decimal Social = gross * .062m;
+            decimal Medi = gross * .029m;
+
+            decimal Net = gross - (State + Federal + Social + Medi);
+            //lblDisplay.Text = gross.ToString("Gross Pay: " + gross + System.Environment.NewLine +
+                                             //"State Deduction: " + State + System.Environment.NewLine + 
+                                            // "Federal Deduction: " + Federal + System.Environment.NewLine +
+                                            // "Social Deduction: " + Social + System.Environment.NewLine +
+                                            // "Medicare Deduction: " + Medi + System.Environment.NewLine +
+                                            // "Net Pay: " + Net);
+
+            lblDisplay.Text = "Gross Pay: " + gross.ToString("c") + System.Environment.NewLine 
+                            + "State Deductions: " + State.ToString("c") + System.Environment.NewLine
+                            + "Federal Deductions: " + Federal.ToString("c") + System.Environment.NewLine
+                            + "Social Security Deductions: " + Social.ToString("c") + System.Environment.NewLine
+                            + "Medicare Deductions: " + Medi.ToString("c") + System.Environment.NewLine
+                            + "Net Income: " + Net.ToString("c");
+        }
+
+
+    }
+}
